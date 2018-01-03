9bcf91d5e34a16231b443a23fd10ee0efa2af4a3
ledomone <ledom.git@gmail.com>
2018-01-03 23:22:40 +0100

    Another delegate with sum

--- Episode4/Models/Functions.cs
+++ Episode4/Models/Functions.cs
@@ -5,11 +5,20 @@ namespace Episode4.Models
     public class Delegates
     {
         public delegate void Write(string message);
+        public delegate int Add(int x, int y);
 
         public void Test()
         {
             Write writer = WriteMessage;
+            Add adder = AddTwoNumbers;
             writer("Dominik");
+            var sum = adder(1, 7);
+            Console.WriteLine(sum);
+        }
+
+        public static int AddTwoNumbers(int a, int b)
+        {
+            return a + b;
         }
 
         public static void WriteMessage(string msg)
