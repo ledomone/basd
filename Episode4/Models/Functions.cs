f7b7c1bb722fe0d33cdb21eff82fec66aec6e93d
ledomone <ledom.git@gmail.com>
2018-01-04 01:10:48 +0100

    Delegaty

--- Episode4/Models/Functions.cs
+++ Episode4/Models/Functions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 
 namespace Episode4.Models
 {
@@ -6,6 +7,7 @@ namespace Episode4.Models
     {
         public delegate void Write(string message);
         public delegate int Add(int x, int y);
+        public delegate void Alert(int change);
 
         public void Test()
         {
@@ -14,6 +16,48 @@ namespace Episode4.Models
             writer("Dominik");
             var sum = adder(1, 7);
             Console.WriteLine(sum);
+            CheckTemperature(TooLowAlert, OptimalAlert, TooHighAlert);
+        }
+
+        public static void TooLowAlert(int change)
+        {
+            Console.WriteLine($"Temperature is too low (changed by {change}).");
+        }
+
+        public static void OptimalAlert(int change)
+        {
+            Console.WriteLine($"Temperature is optimal (changed by {change}).");
+        }
+
+        public static void TooHighAlert(int change)
+        {
+            Console.WriteLine($"Temperature is too high (changed by {change}).");
+        }
+
+        public static void CheckTemperature(Alert tooLow, Alert optimal, Alert tooHigh)
+        {
+            var temperature = 10;
+            var random = new Random();
+
+            while(true)
+            {
+                var change = random.Next(-5, 5);
+                temperature += change;
+                Console.WriteLine($"Temperature is at: {temperature} C.");
+                if (temperature <= 0)
+                {
+                    tooLow(change);
+                }
+                else if (temperature > 0 && temperature <= 10)
+                {
+                    optimal(change);
+                }
+                else
+                {
+                    tooHigh(change);
+                }
+                Thread.Sleep(500);
+            }
         }
 
         public static int AddTwoNumbers(int a, int b)
