1d1dd3025142c48e3b4f47eb7c9635a483252a42
ledomone <ledom.git@gmail.com>
2018-01-26 15:16:03 +0100

    Create new app netcore2.0

--- Episode5/Program.cs
+++ Episode5/Program.cs
@@ -1,17 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using Episode5.Models;
 
 namespace Episode5
 {
-    public class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            var enumerations = new Enumerations();
-            enumerations.Test();
+            Console.WriteLine("Hello World!");
         }
     }
 }
