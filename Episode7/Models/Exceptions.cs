3fec90f14d79ed1edae7b402c7bdfc974b003c1e
ledomone <ledom.git@gmail.com>
2018-01-27 21:49:28 +0100

    Episode7

---
+++ Episode7/Models/Exceptions.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace Episode7.Models
+{
+    public class Exceptions
+    {
+        public void Test()
+        {
+            try
+            {
+                User user = new User("user1@email.com", "Secret");
+                user = null;
+                throw new ArgumentNullException(nameof(user));
+
+                //Sign up user...
+                //Email in use
+                throw new Exception("Email in use.");
+            }
+            catch(ArgumentNullException exception)
+            {
+                Console.WriteLine($"Null error: {exception}");
+            }
+            catch(Exception exception)
+            {
+                Console.WriteLine($"An error: {exception}");
+            }
+            finally
+            {
+                Console.WriteLine("Finally here!");
+            }
+
+            Console.WriteLine("OK");
+        }
+    }
+}
---
+++ Episode7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Episode7.Models;
+
+namespace Episode7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parallelism = new Parallelism();
+            parallelism.Test();
+        }
+    }
+}
