1f9157188800791e1ce9c73c7fa86e6d1c1aecff
ledomone <ledom.git@gmail.com>
2018-01-28 16:16:07 +0100

    Setup for tests

--- Episode7/Tests/UserTests.cs
+++ Episode7/Tests/UserTests.cs
@@ -1,3 +1,4 @@
+using System;
 using Episode7.Models;
 using NUnit.Framework;
 
@@ -6,18 +7,39 @@ namespace Episode7.Tests
     [TestFixture]
     public class UserTests
     {
+        public User User;
+
+        [SetUp]
+        public void Setup()
+        {
+            User = new User("user1@email.com", "secret");
+        }
+
         [Test]
         public void  changing_email_should_succeed()
         {
             // Arrange
             var expectedEmail = "user2@email.com";
-            var user = new User("user1@email.com", "secret");
+            
 
             // Act
-            user.SetEmail(expectedEmail);
+            User.SetEmail(expectedEmail);
 
             // Assert
-            Assert.AreEqual(expectedEmail, user.Email);
+            Assert.AreEqual(expectedEmail, User.Email);
+        }
+
+        [Test]
+        public void providing_empty_password_should_fail()
+        {
+            // Arrange
+            
+            // Act
+            var exception = Assert.Throws<Exception>(() => User.SetPassword(string.Empty));
+
+            // Assert
+            Assert.NotNull(exception);
+            Assert.IsTrue(exception.Message.Equals("Password is incorrect."));
         }
     }
 }
