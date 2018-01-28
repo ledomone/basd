6fe7f15f78ed8f5d4178a3b34e885a4893cc90cb
ledomone <ledom.git@gmail.com>
2018-01-28 14:03:13 +0100

    Nunit dummy test

---
+++ Episode7/Tests/Tests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace Episode7.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Dummy()
+        {
+            Assert.True(true);
+        }
+    }
+}
