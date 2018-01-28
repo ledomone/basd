5ece2d95bbe712ee99b52b25fc5ee8ff7ada1ca7
ledomone <ledom.git@gmail.com>
2018-01-28 17:44:42 +0100

    Test async - HttpClient

---
+++ Episode7/Tests/WebTests.cs
@@ -0,0 +1,34 @@
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Episode7.Models;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+
+namespace Episode7.Tests
+{
+    public class WebTests
+    {
+        public HttpClient HttpClient;
+
+        [SetUp]
+        public void Setup()
+        {
+            HttpClient = new HttpClient();
+        }
+
+        [Test]
+        public async Task http_response_should_have_204_status_code()
+        {
+            // Arrange
+            
+            // Act
+            var response = await HttpClient.GetAsync("http://httpstat.us/204");
+
+            // Assert
+            response.Should().NotBeNull();
+            response.StatusCode.ShouldBeEquivalentTo(HttpStatusCode.NoContent);
+        }
+    }
+}
